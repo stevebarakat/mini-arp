6f167ad37a8bd043e5d817ccdb70e3ec7d58b752
stevebarakat <stevebarakat@gmail.com>
2025-03-11 04:43:16 -0400

    ci

--- src/machines/sequencerMachine.ts
+++ src/machines/sequencerMachine.ts
@@ -6,11 +6,13 @@ type SequencerEvent =
   | { type: "PLAY" }
   | { type: "STOP" }
   | { type: "UPDATE_NOTE"; note: string }
+  | { type: "UPDATE_PITCH"; pitch: number }
   | { type: "SET_GRID"; grid: Grid };
 
 type SequencerContext = {
   note: string;
   grid: Grid;
+  pitch: number;
 };
 
 export const sequencerMachine = createMachine({
@@ -22,6 +24,7 @@ export const sequencerMachine = createMachine({
   },
   context: {
     note: "C4",
+    pitch: 0,
     grid: Array(NOTES.length)
       .fill(null)
       .map(() => Array(STEPS).fill(false)) as Grid,
@@ -42,13 +45,19 @@ export const sequencerMachine = createMachine({
     UPDATE_NOTE: {
       actions: assign(({ context, event }) => ({
         ...context,
-        note: event.type === "UPDATE_NOTE" ? event.note : context.note,
+        note: event.note,
+      })),
+    },
+    UPDATE_PITCH: {
+      actions: assign(({ context, event }) => ({
+        ...context,
+        pitch: event.pitch,
       })),
     },
     SET_GRID: {
       actions: assign(({ context, event }) => ({
         ...context,
-        grid: event.type === "SET_GRID" ? event.grid : context.grid,
+        grid: event.grid,
       })),
     },
   },
